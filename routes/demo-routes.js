fb3525254cbe4aace9097b2383c3a5565995115b
tranbaoan9999 <antranbao2@gmail.com>
2020-06-24 22:01:16 +0700

    Nomal

--- routes/demo-routes.js
+++ routes/demo-routes.js
@@ -5,7 +5,4 @@ const router = express.Router();
 router.get('/about', async function (req, res) {
   res.render('vwDemo/about');
 });
-router.post('/about', async function(req, res){
-  console.log('An love Ty');
-});
 module.exports = router;
