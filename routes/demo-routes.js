cca453ffc1d96d7e5f6848864976b436cdc01c14
tranbaoan9999 <antranbao2@gmail.com>
2020-06-24 21:58:02 +0700

    add post about

--- routes/demo-routes.js
+++ routes/demo-routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 router.get('/about', async function (req, res) {
   res.render('vwDemo/about');
-})
-
+});
+router.post('/about', async function(req, res){
+  console.log('An love Ty');
+});
 module.exports = router;
