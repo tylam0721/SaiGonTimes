ca2ba31e0520bdd88909c971193b3aa3206658e3
lamnhatty <lamnhatty2109@gmail.com>
2020-07-22 21:39:03 +0700

    Update writer page

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(async function (req, res,next) {
 app.use('/',require('./routes/home.routes'));//home route
 app.use('/demo', require('./routes/demo-routes'));//
 app.use('/account', require('./routes/account.routes'));//account route
+app.use('/writer', require('./routes/writer.routes'));
+
 //throw error:
 app.get('/err', function (req, res) {
   throw new Error('beng beng');
