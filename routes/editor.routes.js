e726a9c1147ac6eef6e7052f54826c1f5adafa4f
lamnhatty <lamnhatty2109@gmail.com>
2020-08-06 02:13:28 +0700

    asdasd

--- routes/editor.routes.js
+++ routes/editor.routes.js
@@ -8,27 +8,28 @@ router.get('/', async function(req, res) {
     const editor = await editorModel.select();
     let listStatus = await postModel.loadstatuspost()
     res.render('vwEditor/home', {
-        editor,listStatus
+        editor,
+        listStatus
     });
 });
 router.post('/', async function(req, res) {
-    if(req.body.statusid==3)
-    { 
+    if (req.body.statusid == 3) {
         //await postModel.updateStatus({Status:req.body.statusid}, {PostID:req.body.postid});
-        var d=new Date();
-        today=d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate()+' '+(d.getHours()+1)+':'+(d.getMinutes()+1)+':'+((d.getSeconds()+1));
-        var data={
-            StatusID:req.body.statusid,
-            UserID:3, PostID:req.body.postid,
-            Reason:req.body.reason,
-            Date:today
+        var d = new Date();
+        today = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + (d.getHours() + 1) + ':' + (d.getMinutes() + 1) + ':' + ((d.getSeconds() + 1));
+        var data = {
+            StatusID: req.body.statusid,
+            UserID: 3,
+            PostID: req.body.postid,
+            Reason: req.body.reason,
+            Date: today
         }
         console.log(data);
         await editorModel.updatereason(data);
         res.render('vwEditor/home');
-    }
-    else {
+    } else {
         res.render('vwEditor/home');
     }
 });
 module.exports = router;
+asdasdasdas
